2158598cb592f74103636ba56ab5e60504262db6
Ivan Bongiovanni <bongiovanniivan12@gmail.com>
2023-07-04 04:23:51 -0300

    add billboards and images

--- app/(root)/layout.tsx
+++ app/(root)/layout.tsx
@@ -1,6 +1,7 @@
-import prismadb from '@/lib/prismadb'
-import { auth } from '@clerk/nextjs'
 import { redirect } from 'next/navigation'
+import { auth } from '@clerk/nextjs'
+
+import prismadb from '@/lib/prismadb'
 
 export default async function SetupLayout({ children }: { children: React.ReactNode }) {
   const { userId } = auth()
@@ -9,7 +10,11 @@ export default async function SetupLayout({ children }: { children: React.ReactN
     redirect('/sign-in')
   }
 
-  const store = await prismadb.store.findFirst({ where: { userId } })
+  const store = await prismadb.store.findFirst({
+    where: {
+      userId,
+    },
+  })
 
   if (store) {
     redirect(`/${store.id}`)
